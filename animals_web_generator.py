c95761aea6514a7262b88cfe91c051a4a36dcd6d
Lais  Trevizan Domiciano <laistdomiciano@gamil.com>
2024-04-22 04:59:14 -0300

    Creation of serialize_animal(animal_obj) function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -10,6 +10,7 @@ animals_data = load_data('animals_data.json')
 #print (load_data("animals_data.json"))
 
 def generate_animals_html(output):
+  """ Reads original HTML file and then open a new one """
   with open('animals_template.html', 'r') as file:
     html_content = file.read()
 
@@ -18,11 +19,9 @@ def generate_animals_html(output):
   with open('animals.html', 'w') as file:
     file.write(updated_html_content)
 
-def main ():
-  animals_data = load_data('animals_data.json')
-
-  output = ''  # define an empty string
-
+def serialize_animal(animal_obj):
+  """ Loops through the animal info and creates outputs for the cards in the HTML page """
+  output = ""
   for animal in animals_data:  # loop for getting the animals data in a list of dictionaries
     # append information to each string
     output += '<li class="cards__item">'
@@ -31,7 +30,7 @@ def main ():
     output += f"<strong>Diet:</strong> {animal['characteristics'].get('diet')}<br/>\n"
     locations = animal['locations']
     output += f"<strong>Location:</strong> {locations[0]}<br/>\n"
-    if animal["characteristics"].get("type") is not None:
+    if animal["characteristics"].get("type") is not None: #just gets types that have valid information
       output += f"<strong>Type:</strong> {animal['characteristics'].get('type')}<br/>\n\n"
     else:
       output += "<br/>\n"
@@ -39,7 +38,16 @@ def main ():
     output += '</li>'
 
   generate_animals_html(output)
-  print (output)
+  return output
+
+  generate_animals_html(output)
+  return output
+
+def main ():
+  animals_data = load_data('animals_data.json')
+  output = serialize_animal(animals_data)
+  print(output)
+
 
 if __name__ == "__main__":
   main()
