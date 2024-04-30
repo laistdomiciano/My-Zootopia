ee7e9d387801e50cb7b9c57c7bb86c1343317e07
Lais  Trevizan Domiciano <laistdomiciano@gamil.com>
2024-04-30 10:48:10 -0300

    New Project Architecture

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,13 +1,4 @@
-import json
-
-def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
-
-animals_data = load_data('animals_data.json')
-
-#print (load_data("animals_data.json"))
+import data_fetcher
 
 def generate_animals_html(output):
   """ Reads original HTML file and then open a new one """
@@ -19,50 +10,40 @@ def generate_animals_html(output):
   with open('animals.html', 'w') as file:
     file.write(updated_html_content)
 
-def serialize_animal(animal_obj):
+
+def serialize_animal(animals_data, animal_name):
   """ Loops through the animal info and creates outputs for the cards in the HTML page """
   output = ""
-  for animal in animals_data:  # loop for getting the animals data in a list of dictionaries
+  if animals_data:
+    for animal in animals_data:  # loop for getting the animals data in a list of dictionaries
     # append information to each string
-    output += '<li class="cards__item">'
-    output += f"<div class=\"card__title\">{animal['name']}</div>\n"
-    output += '<p class="card__text">'
-    output += f"<strong>Diet:</strong> {animal['characteristics'].get('diet')}<br/>\n"
-    locations = animal['locations']
-    output += f"<strong>Location:</strong> {locations[0]}<br/>\n"
-    if animal["characteristics"].get("type") is not None: #just gets types that have valid information
-      output += f"<strong>Type:</strong> {animal['characteristics'].get('type')}<br/>\n\n"
-    else:
-      output += "<br/>\n"
-    output += '</p>'
-    output += '</li>'
+      output += '<li class="cards__item">'
+      output += f"<div class=\"card__title\">{animal['name']}</div>\n"
+      output += '<p class="card__text">'
+      output += f"<strong>Diet:</strong> {animal['characteristics'].get('diet')}<br/>\n"
+      locations = animal['locations']
+      output += f"<strong>Location:</strong> {locations[0]}<br/>\n"
+      if animal["characteristics"].get("type") is not None: #just gets types that have valid information
+        output += f"<strong>Type:</strong> {animal['characteristics'].get('type')}<br/>\n\n"
+      else:
+        output += "<br/>\n"
 
+      output += '</p>'
+      output += '</li>'
+
+    print("Website was successfully generated to the file animals.html")
+  else:
+    output += f"<h2>The animal \"{animal_name}\" doesn't exist.</h2>"
+    print(f"The animal \"{animal_name}\" doesn't exist.")
   generate_animals_html(output)
   return output
 
-  generate_animals_html(output)
-  return output
 
 def main ():
-  animals_data = load_data('animals_data.json')
-  output = serialize_animal(animals_data)
-  print(output)
+  animal_name = input('Enter a name of an animal: ')
+  animals_data = data_fetcher.fetch_data(animal_name)
+  output = serialize_animal(animals_data, animal_name)
 
 
 if __name__ == "__main__":
   main()
-
-
-# for animal in animals_data: #loop for getting the animals data in a list of dictionaries
-#   name = animal["name"]
-#   diet = animal["characteristics"].get("diet")
-#   locations = animal["locations"]
-#   first_location = locations[0]
-#   animal_type = animal["characteristics"].get("type")
-
-# print(f"Name: {name}")
-# print(f"Diet: {diet}")
-# print(f"Location:{first_location}")
-# if animal_type is not None:
-#   print(f"Type:{animal_type}")
-# print()
---
+++ data_fetcher.py
@@ -0,0 +1,27 @@
+import requests
+
+API_KEY = '9LJ3lQCJQboTNfAlciFYww==sf7AsjitT8iLVqQL'
+
+def fetch_data(animal_name):
+  """
+  Fetches the animals data for the animal 'animal_name'.
+  Returns: a list of animals, each animal is a dictionary:
+  {
+    'name': ...,
+    'taxonomy': {
+      ...
+    },
+    'locations': [
+      ...
+    ],
+    'characteristics': {
+      ...
+    }
+  },
+  """
+  api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(animal_name)
+  response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+  if response.status_code == requests.codes.ok:
+    return response.json()
+  else:
+    print("Error:", response.status_code, response.text)
