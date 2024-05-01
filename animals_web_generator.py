adb29197362d1c8114a4d8c0c279136306d90148
Lais  Trevizan Domiciano <laistdomiciano@gamil.com>
2024-05-01 13:49:36 -0300

    Last fix of python-dotenv

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,49 +1,47 @@
+import sys
+print(sys.path)
 import data_fetcher
 
 def generate_animals_html(output):
-  """ Reads original HTML file and then open a new one """
-  with open('animals_template.html', 'r') as file:
-    html_content = file.read()
+    """ Reads original HTML file and then open a new one """
+    with open('animals_template.html', 'r') as file:
+        html_content = file.read()
 
-  updated_html_content = html_content.replace('__REPLACE_ANIMALS_INFO__', output)
+    updated_html_content = html_content.replace('__REPLACE_ANIMALS_INFO__', output)
 
-  with open('animals.html', 'w') as file:
-    file.write(updated_html_content)
-
+    with open('animals.html', 'w') as file:
+        file.write(updated_html_content)
 
 def serialize_animal(animals_data, animal_name):
-  """ Loops through the animal info and creates outputs for the cards in the HTML page """
-  output = ""
-  if animals_data:
-    for animal in animals_data:  # loop for getting the animals data in a list of dictionaries
-    # append information to each string
-      output += '<li class="cards__item">'
-      output += f"<div class=\"card__title\">{animal['name']}</div>\n"
-      output += '<p class="card__text">'
-      output += f"<strong>Diet:</strong> {animal['characteristics'].get('diet')}<br/>\n"
-      locations = animal['locations']
-      output += f"<strong>Location:</strong> {locations[0]}<br/>\n"
-      if animal["characteristics"].get("type") is not None: #just gets types that have valid information
-        output += f"<strong>Type:</strong> {animal['characteristics'].get('type')}<br/>\n\n"
-      else:
-        output += "<br/>\n"
+    """ Loops through the animal info and creates outputs for the cards in the HTML page """
+    output = ""
+    if animals_data:
+        for animal in animals_data:
+            output += '<li class="cards__item">'
+            output += f"<div class=\"card__title\">{animal['name']}</div>\n"
+            output += '<p class="card__text">'
+            output += f"<strong>Diet:</strong> {animal['characteristics'].get('diet')}<br/>\n"
+            locations = animal['locations']
+            output += f"<strong>Location:</strong> {locations[0]}<br/>\n"
+            if animal["characteristics"].get("type") is not None:
+                output += f"<strong>Type:</strong> {animal['characteristics'].get('type')}<br/>\n\n"
+            else:
+                output += "<br/>\n"
 
-      output += '</p>'
-      output += '</li>'
+            output += '</p>'
+            output += '</li>'
 
-    print("Website was successfully generated to the file animals.html")
-  else:
-    output += f"<h2>The animal \"{animal_name}\" doesn't exist.</h2>"
-    print(f"The animal \"{animal_name}\" doesn't exist.")
-  generate_animals_html(output)
-  return output
+        print("Website was successfully generated to the file animals.html")
+    else:
+        output += f"<h2>The animal \"{animal_name}\" doesn't exist.</h2>"
+        print(f"The animal \"{animal_name}\" doesn't exist.")
+    generate_animals_html(output)
+    return output
 
-
-def main ():
-  animal_name = input('Enter a name of an animal: ')
-  animals_data = data_fetcher.fetch_data(animal_name)
-  output = serialize_animal(animals_data, animal_name)
-
+def main():
+    animal_name = input('Enter a name of an animal: ')
+    animals_data = data_fetcher.fetch_data(animal_name)
+    output = serialize_animal(animals_data, animal_name)
 
 if __name__ == "__main__":
-  main()
+    main()
--- data_fetcher.py
+++ data_fetcher.py
@@ -7,25 +7,25 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 def fetch_data(animal_name):
-  """
-  Fetches the animals data for the animal 'animal_name'.
-  Returns: a list of animals, each animal is a dictionary:
-  {
-    'name': ...,
-    'taxonomy': {
-      ...
+    """
+    Fetches the animals data for the animal 'animal_name'.
+    Returns: a list of animals, each animal is a dictionary:
+    {
+        'name': ...,
+        'taxonomy': {
+            ...
+        },
+        'locations': [
+            ...
+        ],
+        'characteristics': {
+            ...
+        }
     },
-    'locations': [
-      ...
-    ],
-    'characteristics': {
-      ...
-    }
-  },
-  """
-  api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(animal_name)
-  response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
-  if response.status_code == requests.codes.ok:
-    return response.json()
-  else:
-    print("Error:", response.status_code, response.text)
+    """
+    api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(animal_name)
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
+    if response.status_code == requests.codes.ok:
+        return response.json()
+    else:
+        print("Error:", response.status_code, response.text)
